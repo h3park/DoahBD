8b0f62647255647839096b537d59af23df42af15
hpark <hpark@sanion.com>
2023-05-24 17:57:29 +0900

    네이버 지도 추가

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,18 @@
       <div class="mainSection">
         <img src="../assets/doahJoah_100.jpg"  class="image-fit" z-index=0>
         <div class="imgBlur">
-          <div style="padding: 50px;">
+          <div style="padding: 20rem;">
             <p style="color:white; font-size: 1.5rem; opacity=1">도아의 돌잔치에</br>초대합니다</p>
           </div>
           
         </div>
       </div>
       <div>
-        <p>세상에서 가장 큰 선물로</p>
+        <p>세상에서 가장 큰 선물로 우리에게 와준 </br>도아쨩</p>
       </div>
     </div>
     <div class="dirSection">
-      <div id="map" ref="map" style="width:100%;height:400px;"></div>  
+      <div id="map" style="width:50%;height:450px;margin: 5rem auto"></div>  
     </div>
     
 
@@ -24,23 +24,39 @@
 
 <script>
 export default {
-  beforeMount() {
-    let naver = document.createElement('script');
-    naver.setAttribute('src',"https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=i1bmdsyj4j");
-    document.head.appendChild(naver);
-
-    this.$refs.map = new naver.maps.Map('map',{       
-      center: new naver.maps.LatLng(37.3595704, 127.105399),
-      zoom: 10
+  name: 'IndexPage',
+  mounted(){    
+    // 네이버 지도 추가
+    const script = document.createElement('script');
+    script.src = 'https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=i1bmdsyj4j';
+    
+    script.addEventListener("load", () =>{
+      var map = new naver.maps.Map('map',{
+        center: new naver.maps.LatLng(37.3595704, 127.105399), //지도의 초기 중심 좌표
+        zoom: 15, 
+         minZoom: 7, //지도의 최소 줌 레벨
+        zoomControl: true, //줌 컨트롤의 표시 여부
+        zoomControlOptions: { //줌 컨트롤의 옵션
+            position: naver.maps.Position.TOP_RIGHT
+        }
+      })
+      var marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(37.3595704, 127.105399),
+        map: map
+      })
     });
+    
+    document.head.appendChild(script);
+    
   },
-  mounted(){
-},
   data(){
     return{
+      lat: 0,
+      long: 0,
     } 
-  methods:{
-  }
+  // methods:{
+
+  // }
   },
 }
 </script>
