c751e310ed4853fc9639d21b4dfcf72d72375b8d
hpark <hpark@sanion.com>
2023-07-25 18:01:14 +0900

    072523 | 갤러리 & 달력 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
     'bootstrap-vue/nuxt',
   ],
   bootstrapVue:{
-    icon:true,
+    icons:true,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
--- pages/index.vue
+++ pages/index.vue
@@ -1,131 +1,159 @@
 <template>
   <div id="container">
     <!-- 메인 -->
-    <div class="mainSection ">
-      <img src="../assets/doahJoah_100.jpg"  class="image-fit" z-index=0>
+    <div class="mainSection full">
+      <!-- <div style="width:100%; height:100%">
+        <img src="../assets/mainBack.jpg" class="image-fill">
+      </div> -->
+      <div style="width:100%; height:100%; position: relative;">
+        <img src="../assets/mainBack6.png" class="image-fit">
+      </div>
+
+      <!-- <h1 class="paragraph temp-abs" style="top:0;">도아의 첫번째 생일</h1> -->
+      <div class="temp-abs" style="height:23%; top:100px">
+        <img src="../assets/words/mainTitle.png" class="image-contain">
+      </div>
+      <div class="temp-abs" style="height:60%; top: 23%">
+        <img src="../assets/MAIN1.png" class="mainImg image-contain">
+      </div>
+      <div class="paragraph temp-abs" style="height: 17%; top:83%">
+        <h4>2023.08.23 12시</h4>
+        <h4>장소</h4>
+      </div>
+      <!-- <img src="../assets/doahJoah_100.jpg"  class="image-fit" z-index=0>
       <div class="imgBlur">
         <div style="padding: 2rem;">
           <p style="color:white; font-size: 1.5rem; opacity=1">도아의 돌잔치에<br>초대합니다</p>
+        </div>        
+      </div> -->
+    </div>
+    
+    <!-- 초대장 -->
+    <div class="invitationSection full" style="height:100%; display: block" >
+      <div class="invitationCard">
+        <img src="../assets/invitationTop.png" class="image-fill" style="height:5%;">
+        <img src="../assets/words/invitation1.png" class="image-contain sub-title-image">
+        <p class="paragraph card-content size-limit">열 달을 기다려 새로운 생명을 만났습니다.
+          작은 생명의 꼬물거리는 움직임을 보면서 한엇이 감사했습니다.
+          그렇게 저희 부부에게로 온 도아가 어느새 첫 번째 생일을 맞습니다.
+          그동안 도아를 아끼고 사랑해주신 분들께 감사의 마음으로 작은 잔치를 마련했습니다.
+          꼭 참석하셔서 함께 기뻐해주시기 바람니다.
+        </p>
+        <!-- 연락처 -->
+        <div class="contactSection size-limit" style="width:65%">
+          <div v-for="(telNum, p) in parentTel" :key="p" class="singleContact">
+            <h4 style="display:flex; align-items:bottom; ">{{p}}에게 연락하기</h4>
+            <div class="contactPhone">
+              <button :href="'tel:'+telNum">
+                <b-icon icon="telephone-fill" :style="[p==='아빠'? 'color: #89cff0;':'color:#f4c2c2;']"></b-icon></button>
+              <span class="divider" :style=" p === '아빠' ? 'background-color:#89cff0': 'background-color: #f4c2c2'"></span>
+              <button :href="'sms:'+telNum">
+                <b-icon icon="envelope-fill" :style="[p==='아빠'? 'color: #89cff0':'color:#f4c2c2']"></b-icon>
+              </button>
+            </div>
+          </div>
         </div>
-        
-      </div>
-    </div>
-    <!-- 사진 -->
-    <div style="height:100vh">
-      <h3>VIEW PHOTOS</h3>
-      <div style="height:90%;">
-        <img :src="require('../assets/gallery/gallery'+currImgIndex+'.jpg')"
-          class="main-img"
-          v-on:mouseover="Hover"
+        <!-- 달력? -->
+        <div class="calendarSection">
+          <b-calendar
+            id="my_calendar"
+            ref="calendar"
+            disabled=true
+            hide-header=true
+            no-key-nav=true
+            label-help=""
+            value="2023-08-23"
+            readonly=true
+            selected-variant="info"
+            block 
           >
-        <carousel ref="photoList" id="photoList"
-          :items="10"
-          :margin="6"
-          :freedrag="true" 
-          :nav="false" 
-          :loop="true"
-          :center="true"
-          :dots="false"
-          :auto-height="false"
-          :rewind="false"
-          :startPosition="0"
-          :autoplay="autoplayFlag"
-          :autoplayTimeout="autoplayTime"
-          :autoplayHoverPause="true"
-          @changed="changed"
-          @update="updated"
-        >
-          <img v-for="(_,index) in photoCnt" 
-            :key="index" 
-            :src="require('../assets/gallery/gallery'+index+'.jpg')"
-            @click="GoTo(index)"
-            class="image-fit">
-        </carousel>
-      </div>
-      <!-- 하단 -->
-      
-
-      <div>
-
+          </b-calendar>
+          <!-- 까지 며칠 남았습니다 -->
+        </div>
       </div>
     </div>
 
-    <!-- 인사? -->
-    <div>
-      <p>{{}}</p>
-    </div>
-
-    <!-- 달력? -->
-    <div>
-      <b-calendar
-        id="my_calendar"
-        ref="calendar"
-        disabled=true
-        hide-header=true
-        no-key-nav=true
-        label-help=""
-        value="2023-08-23"
-        readonly=true
-        selected-variant="info"
-        block 
-      >
-      </b-calendar>
-      <!-- 까지 며칠 남았습니다 -->
+    <!-- 사진 -->
+    <div class="gallerySection full">
+      <div></div>
+      <img src="../assets/words/gallery.png" class="image-contain sub-title-image">
+      <div style="height:calc(100% - 22vmin); padding-bottom:3rem;">
+        <!-- <img :src="require('../assets/gallery/gallery'+currImgIndex+'.jpg')"
+          class="main-img"
+          @mouseover="Hover(true)" @mouseleave="Hover(false)"> -->
+        <hooper group="group1" style="height: 85%; padding-bottom:1.5rem;" class="size-limit" :wheelControl="false" :infiniteScroll="true">
+          <slide
+            v-for="(_,index) in photoCnt" 
+            :key="index" >
+            <img                  
+              :src="require('../assets/gallery/gallery'+index+'.jpg')"              
+              class="main-img"
+              @mouseover="Hover(true)" @mouseleave="Hover(false)"
+            >              
+          </slide>     
+          <hooper-navigation slot="hooper-addons"></hooper-navigation>       
+        </hooper>
+        <hooper group="group1" ref="gallery" class="sub-photo-gallery size-limit"
+          :itemsToShow="8" :centerMode="true" :infiniteScroll="true" :wheelControl="false"
+          @slide="updateCarousel({$event})" 
+          @updated="UPD" >
+          <slide
+            v-for="(_,index) in photoCnt" 
+            :key="index" >
+            <img                  
+              :src="require('../assets/gallery/gallery'+index+'.jpg')"
+              @click="ImageClicked($event,index)"
+              class="sub-img">              
+          </slide>            
+        </hooper>
+      </div>
     </div>
 
     <!-- 지도 -->
-    <div class="dirSection">
-      <p>위치</p>
-      <div class="mapUI">
+    <div class="dirSection full">
+      <img src="../assets/words/location.png" class="image-contain sub-title-image">
+      <div class="dirContent size-limit" style="width: 70%; max-width: 800px;">
         <div id="map"></div>  
+        <!-- <b-button squared
+          href="https://map.naver.com/v5/directions/-/14138002.419104088,4516506.529278252,%EC%84%9C%EC%9A%B8%ED%81%B4%EB%9F%BD,20202001,PLACE_POI/-/transit?c=15,0,0,0,dh">지도 열기</b-button> -->
+        
         <b-button squared
-          style="width:100%"
-          href="https://map.naver.com/v5/directions/-/14138002.419104088,4516506.529278252,%EC%84%9C%EC%9A%B8%ED%81%B4%EB%9F%BD,20202001,PLACE_POI/-/transit?c=15,0,0,0,dh">지도 열기</b-button>
-      </div>
-      <div>
-        <p style="font-weight: bold">{{placeName}}<span style="font-size:0.9rem; font-weight: normal">{{placeNameDetail.length > 0 ? '/':''}}{{placeNameDetail}}</span></p>
-        <p>{{address}}</p>
-        <span>{{placeTel}}</span>
+          href="https://naver.me/GZAjsyPn">지도 열기</b-button>
+        
+        <div class="description">
+          <p style="font-weight: bold">{{placeName}}<span style="font-size:0.9rem; font-weight: normal">{{placeNameDetail.length > 0 ? '/':''}}{{placeNameDetail}}</span></p>
+          <p>{{address}}</p>
+          <p>{{placeTel}}</p>
 
-        <!-- guide -->
-        <div>
-          <ul>
-            <li v-for="(des, vehicle) in transport" :key="vehicle">
-              <div>{{vehicle}}</div>
-              <div style="white-space: pre-line">{{des.join('\n')}}</div>
-            </li>
-          </ul>
+          <!-- guide -->
+          <div>
+            <ul>
+              <li v-for="(des, vehicle) in transport" :key="vehicle">
+                <div>{{vehicle}}</div>
+                <div class="paragraph">{{des.join('\n')}}</div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
-       
-
+      <img src="../assets/locationBG.png" class="location-img">
     </div>
       
-    <!-- 연락처 -->
-    <div class="contactSection">
-      <div v-for="(telNum, p) in parentTel" :key="p" class="singleContact">
-        <p>{{p}}에게 연락하기</p>
-        <div class="contactPhone">
-          <button :href="'tel:'+telNum">
-            <b-icon icon="telephone-fill" :style="[p==='아빠'? 'color: #89cff0;':'color:#f4c2c2;']"></b-icon></button>
-          <button :href="'sms:'+telNum">
-            <b-icon icon="envelope-fill" :style="[p==='아빠'? 'color: #89cff0':'color:#f4c2c2']"></b-icon>
-          </button>
-        </div>
-      </div>
-    </div>
-    <!-- 공유하기? -->
+
   </div>
+  
 </template>
 
 <script>
-import carousel from 'vue-owl-carousel'
+import { Hooper, Slide,   Navigation as HooperNavigation } from 'hooper';
+import 'hooper/dist/hooper.css';
 
 export default {
   name: 'IndexPage',
   components: {
-    carousel,
+    Hooper,
+    Slide,
+    HooperNavigation
   },
 
   mounted(){    
@@ -163,19 +191,16 @@ export default {
     let isAndroidDevice = isAndroid.test(details);
 
     console.log("========test==========")
-    if (isAppleDevice) {
+    if (isAppleDevice || isAndroidDevice) {
         console.log("You are using a Apple Device");
-    }else if(isAndroidDevice){
-        console.log("You are using a Android Device");
-    } 
+    }
+    // }else if(isAndroidDevice){
+    //     console.log("You are using a Android Device");
+    // } 
     else {
         console.log("You are using Desktop");
     }
 
-    console.log("===")
-    // console.log(this.$refs.calendar)
-    // this.$refs.calendar.activeYMD = "2023-08-23"
-    // this.$refs.calendar.selectedDate = "2023-08-23"
   },
   data(){
     return{
@@ -186,17 +211,14 @@ export default {
       placeName: "서울클럽",
       placeNameDetail: "",
       placeTel: "02-2238-7666",
-      transport: {"지하철":["동대문","3번 출구"], "버스":[], "승용차":[]},
+      transport: {"지하철":[], "버스":[], "승용차":[]},
       parentTel:{"아빠":"01030365213", "엄마": "01028269110"},
       birthDay: "2023-08-23",
       calendarContext: null,
-      slide: 0,
-      sliding: null,
       photoCnt: 30,
       dragged: false,
       currImgIndex: 0,
-      autoplayFlag: true,
-      autoplayTime: 1000,
+      items: 7,
     }
   },
   methods:{
@@ -205,82 +227,152 @@ export default {
             console.log("===")
     console.log(this.$refs.calendar)
     },
-    changed(){
-      // this.dragged = true;
-      console.log('changed')
-      //       return
-
-      // console.log(this.$refs.photoList)
-    },
-    updated(){
-      console.log('updated')
-      console.log(this.$refs.photoList)
-    },
-    ImageClicked(index){
-    },
-    Hover(){
-            console.log(this.$refs.photoList)
-
-      this.autoplayFlag = false
-      this.$refs.photoList.autoplay = false
-  console.log("==============")
-console.log(this.$refs.photoList.$props["get autoplay"])
+    ImageClicked(e,index){
+      console.log(e)
+      let slide = structuredClone(this.$refs.gallery.isSliding)
+      console.log("clicked")
+      console.log(this.$refs.gallery.isSliding)
+      if(!slide) {
+        this.currImgIndex = index;
+        this.$refs.gallery.slideTo(index)
+      }
       
-      console.log(this.$refs.photoList.$data.showNext)
-      // this.$refs.photoList_props.autoplay(false);
-      // this.$refs.photoList.$porps.autoplay = false
-
-      
-    }
+      e.preventDefault()
+    },
+    BTNClicked(){
+      $('.next').click(function() {
+          owl.trigger('stop.owl.autoplay');
+      })
+    },
+    UPD(){
+      console.log("UPDATED")
+    },
+    updateCarousel(payload){
+      console.log(this.$refs.gallery.isDragging)
+      console.log("Update")
+      console.log(payload)
+      console.log(this.$refs.gallery)
+      this.currImgIndex = payload.currentSlide%this.photoCnt
+      this.dragged = true
+    },
+    Hover(val){
+      this.$refs.gallery.isHover = val
+      console.log(this.$refs.gallery.isHover)
+    },
   }
 }
 </script>
+
 <style>
 body {
     min-width: 330px
 }
+
 #container {
   width: 100%;
   position: relative;
   outline: none;
   flex: 1 1 0%;
-  background-color: #ffe5eb;
+  /* background-color: #E3F4EA; */
   overflow-y: auto;
   font-family: 'Sunflower', sans-serif;
+  /* padding-bottom: 7rem; */
 }
-#container::-webkit-scrollbar {
-  display: none;
+
+body::-webkit-scrollbar{
+    display: none;
+    -webkit-appearance: none;
+    overflow: hidden;
 }
-#container .mainSection{
-  width: 100%;
-}
-#container > div{
+
+#container .size-limit{
   width: 86%;
   margin: auto;
-  display: block;
+  max-width: 900px;
+  min-width: 300px;
 }
-#container h3{
+
+#container h2{
+  margin: 80px 0 30px 0;
   /* margin-top: 100px; */
   text-align: center;
   font-weight: bold;
 }
+
+.paragraph{
+  white-space: pre-line;
+}
+
 .full{
-  width: 100vw;
-  height: 100vh;
-}
-.mainSection{
-  width: 100vw;
+  width: 100%;
   height:100vh;
-  background-color: antiquewhite;
+  min-height: 600px;
 }
+
+/* 이미지 */
 .image-fit{
   height: 100%;
   width: 100%;
   object-fit: cover;
 }
-.cover{
-  object-fit: cover;
+.image-fill{
+  height: 100%;
+  width: 100%;
+  object-fit: fill;
 }
+.image-contain{
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+  display: block;
+  margin: auto;
+  padding: 8px;
+}
+.sub-title-image{
+  height: 25vmin;
+  min-height: 100px;
+  width: 35%;
+  max-width: 500px;
+  display: block;
+  margin: 0 auto;
+  padding: 5vmin 0;
+}
+
+
+/* Main */
+.mainSection{
+  background-color: white;
+  position: relative;
+}
+/* 윗 글 */
+.mainSection h1{
+  margin: 0;
+  height: 23%;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+}
+.mainSection .temp-abs{
+  position: absolute;
+  width:100%;
+}
+/* 이미지 */
+.mainImg{
+  height: 100%;
+  display: block;
+  margin: auto;
+}
+/* 아래 */
+.mainSection h4{
+  text-align: center;
+}
+/* .mainContainer{
+  position: absolute;
+  top: 0;
+} */
+
+
+
 
 .imgBlur{
   background-color:black; 
@@ -292,77 +384,51 @@ body {
   z-index: 1;
 }
 
-
-/* 갤러리 */
-/* #photoList div{
-  min-height: 150px;
-  max-height: 250px;
-} */
-#photoList{
-  height: 20%;
+/* 초대장 */
+.invitationSection{
+  position:relative;
 }
-.main-img{
-  height:80%; 
-  min-height: 500px;
-  display: block;
-  margin: auto;
-  padding-bottom: 1rem;
+.invitationCard{
+  min-height: 100vh;
+  height: 100%;
+  width: 100%;
 }
-#photoList .owl-stage > div{
-  aspect-ratio: 1/1;
+.invitationCard .card-content{
+  line-height: 10vmin;
+  font-size: 3vmin;
+  text-align: center;
+  margin: 5vmin auto !important;
 }
 
-/* 달력 */
-#my_calendar__calendar-nav_{
-  position: absolute;
-  height: 1px;
-  overflow: hidden;
-}
-#my_calendar__calendar-grid_{
-  max-width: 650px;
-  margin: auto;
-}
-#my_calendar__calendar-grid_ .text-muted{
-  color: black !important;
-}
-#my_calendar__calendar-grid_ .text-dark{
-  color: black !important;
-}
-#my_calendar__calendar-grid_ .b-calendar-grid-body .btn-info{
-  opacity: 1 !important;
-  background-color: #fc5a8d;
-}
-
-.b-calendar-grid-body .no-gutters > .col, .no-gutters > [class*="col-"]{
-  aspect-ratio: 1 / 0.9;
-}
-
-/* 지도 */
-.dirSection .mapUI #map{
-  max-width:650px; 
-  width: 100%; 
-  /* min-width:300px;  */
-  aspect-ratio: 1 / 0.7; 
-  min-height:300px;
-  margin: auto
-}
-.dirSection .mapUI a{
-  display: block;
-  margin: auto;
-  max-width: 650px;
-  /* min-width: 300px */
-}
 /* 연락처 */
 .contactSection{
   display: flex;
-  flex-direction: row;;
+  flex-direction: row;
 }
-.singleContact{
+.contactSection .singleContact{
   display: flex;
   flex-direction: column;
   flex: 1;
-  border: 3px solid;
-  width:50%;
+  /* border: 3px #507a6b solid; */
+  /* background-color: #E7F5ED; */
+  /* background-color: #C7E9D6; */
+  aspect-ratio: 1/0.5;
+  width: calc(50% - 20px);
+  height: 5%;
+  min-height: 100px;
+  margin: 10px;
+}
+.singleContact h4{
+  font-size: 3.3vmin;
+  height: 20%;
+  align-items: end;
+  justify-content: center;
+  text-shadow: 2px 1px 2px gray;
+}
+.singleContact .divider{
+  height: 36%;
+  width: 0.2vw;
+  margin: auto;
 }
 .contactPhone{
   display: flex;
@@ -372,11 +438,158 @@ body {
 .singleContact .contactPhone button{
   background-color: transparent; 
   border: none; 
-  font-size:0.9rem
+  font-size:0.9rem;
+  flex: 1;
 }
 .singleContact .contactPhone svg{
-  width: 30px;
-  height: 30px;
+  width: 35%;
+  max-width: 60px;
+  height: 35%;
+  max-height: 60px;
+}
+
+/* 갤러리 */
+@media all and (orientation: landscape){
+  .gallerySection{
+    height: 100vh;
+    min-height: 800px;
+    background-color: #E7F5ED;
+  }
+  .gallerySection .sub-img{
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    /* opacity: 0.7; */
+    aspect-ratio: 1/1;
+  }
+}
+@media all and (orientation: portrait){
+  .gallerySection{
+    height: 100vh;
+    min-height: 600px;
+    background-color: #E7F5ED;
+  }
+  .gallerySection .sub-img{
+    width: 100%;
+    object-fit: cover;
+    /* opacity: 0.7; */
+    aspect-ratio: 1/1;
+  }
+}
+
+.gallerySection .main-img{
+  height:100%; 
+  width:100%;
+  display: block;
+  margin: auto;
+  object-fit: contain;
+}
+.gallerySection .sub-photo-gallery{
+  height: 15%;
+}
+.gallerySection .hooper-slide{
+  padding: 0 2px;
+  opacity: 0.7;
+}
+.gallerySection .is-current{
+  /* border: 2px blue solid !important; */
+  opacity: 1 !important;
+  padding:0 2px;
+}
+.gallerySection .sub-photo-gallery .is-current img{
+  outline: 3px #ffd667 solid; outline-offset:-3px
+}
+
+
+/* 달력 */
+@media all and (orientation: landscape){
+  #my_calendar__calendar-grid_{
+    max-width: 750px;
+    width:50%;
+    margin: auto;
+  }
+}
+@media all and (orientation: portrait){
+  #my_calendar__calendar-grid_{
+    max-width: 750px;
+    width:80%;
+    min-width: 300px;
+    margin: auto;
+  }
+}
+.calendarSection{
+  padding-bottom: 5vmin;
+}
+#my_calendar__calendar-nav_{
+  position: absolute;
+  height: 1px;
+  overflow: hidden;
+}
+
+#my_calendar__calendar-grid_ .text-muted{
+  color: black !important;
+}
+#my_calendar__calendar-grid_ .text-dark{
+  color: black !important;
+}
+#my_calendar__calendar-grid_ .b-calendar-grid-body .btn-info{
+  opacity: 1 !important;
+  background-color: #3CB39B;
+}
+
+.b-calendar-grid-body .no-gutters > .col, .no-gutters > [class*="col-"]{
+  aspect-ratio: 1 / 0.9;
+}
+
+/* 지도 */
+.dirSection .dirContent{
+  width:70%;
+  max-width: 800px;
+  margin: auto;
+  min-width: 300px;
+}
+.dirSection .dirContent #map{
+  /* min-width:300px;  */
+  aspect-ratio: 1 / 0.7; 
+  min-height:100px;
+  margin: auto
+}
+.dirSection .dirContent a{
+  display: block;
+  margin: auto;
+  font-size: 2.8vmin;
+  background-color: #9fd8b8;
+  border-color: #9fd8b8;
+  /* min-width: 300px */
+}
+.dirSection .description{
+  margin: 5vmin 0;
+  /* background-color: rgba(255,255,255, .75); */
+  background-color: rgba(255,255,255, .75);
+  border-radius: 10px;
+}
+.dirSection .description p{
+  margin: 0;
+}
+.dirSection .description .place-name{
+  font-weight: bold;
+  font-size: ;
+}
+@media all and (orientation: landscape){
+  .dirSection .location-img{
+    object-fit: fill;
+    width: 100%;
+    z-index: -1;
+  }
+}
+@media all and (orientation: portrait){
+  .dirSection .location-img{
+    object-fit: fill;
+    position:absolute;
+    bottom:0;
+    width: 100%;
+    z-index: -1;
+  }
 }
 
 </style>>
